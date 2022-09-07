2fe45d8c3bdf74b3e53f7e79927f069f763e0953
Muhammad Fikri Sofyan <kiky.fikri31@gmail.com>
2022-09-07 12:22:22 +0700

    update fitur sellcar

--- src/pages/initialpage/index.js
+++ src/pages/initialpage/index.js
@@ -1,5 +1,5 @@
-import React, { useState, useStyles, useEffect, useRef } from "react";
-import { Banner, Bodywrapper } from "../../components";
+import React, { useState } from "react";
+import { Banner, Bodywrapper, SellCar } from "../../components";
 import { CarListReady, findmobildetail } from "../../services/apiv1";
 import { Carditem } from "./carditem";
 import { Oval } from "react-loader-spinner";
@@ -9,7 +9,6 @@ import Select from "@mui/material/Select";
 import InputLabel from "@mui/material/InputLabel";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import { sampledata } from "../../sampledata";
 
 const Index = () => {
   const [listdatatosell, setlistdatatosell] = React.useState(null);
@@ -17,96 +16,37 @@ const Index = () => {
   const [merekmobildt, setmerekmobildt] = React.useState(null);
   const [hargamobildt, sethargamobildt] = React.useState(null);
   const [warnamobildt, setwarnamobildt] = useState(null);
-  const [inputmerekmbl, setinputmerekmbl] = useState("");
-  const [inputdetailmbl, setinputdetailmbl] = useState("");
-  const [inputtahunmbl, setinputtahunmbl] = useState("");
-  const [inputtipembl, setinputtipembl] = useState("");
-  const [inputmesinmbl, setinputmesinmbl] = useState("");
-  const [inputtransmisimbl, setinputtransmisimbl] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const Sampledata = { sampledata };
-  // console.log(sampledata[0].detail[0].detail_merek);
-
-  const detail_merek_toyota = sampledata[0].detail.map((detail_merek) => detail_merek);
-  const detail_merek_honda = sampledata[1].detail.map((detail_merek) => detail_merek);
-  const detail_merek_mitsubishi = sampledata[2].detail.map((detail_merek) => detail_merek);
-  const detail_spek_toyota_agya = detail_merek_toyota[0].detail_spek.map((spek) => spek);
-  const detail_spek_toyota_avanza = detail_merek_toyota[1].detail_spek.map((spek) => spek);
-  const detail_spek_toyota_rush = detail_merek_toyota[2].detail_spek.map((spek) => spek);
-  const detail_spek_honda_brio = detail_merek_honda[0].detail_spek.map((spek) => spek);
-  const detail_spek_honda_brv = detail_merek_honda[1].detail_spek.map((spek) => spek);
-  const detail_spek_honda_hrv = detail_merek_honda[2].detail_spek.map((spek) => spek);
-  const detail_spek_mitsubishi_xpander = detail_merek_mitsubishi[0].detail_spek.map((spek) => spek);
-  const detail_spek_mitsubishi_outlander = detail_merek_mitsubishi[1].detail_spek.map((spek) => spek);
-  const detail_spek_mitsubishi_pajero = detail_merek_mitsubishi[2].detail_spek.map((spek) => spek);
-
-  const detail_merek = [...detail_merek_toyota, ...detail_merek_honda, ...detail_merek_mitsubishi];
-  const detail_spek_toyota = [...detail_spek_toyota_agya, ...detail_spek_toyota_avanza, ...detail_spek_toyota_rush];
-  const detail_spek_honda = [...detail_spek_honda_brio, ...detail_spek_honda_brv, ...detail_spek_honda_hrv];
-  const detail_spek_mitsubishi = [...detail_spek_mitsubishi_xpander, ...detail_spek_mitsubishi_outlander, ...detail_spek_mitsubishi_pajero];
-  const detail_spek = [...detail_spek_toyota, ...detail_spek_honda, ...detail_spek_mitsubishi];
-  // console.log(detail_spek);
-
-  const setinputmerek = (event) => {
-    console.log("setinputmerek", event.target.value);
-    setinputmerekmbl(event.target.value);
-  };
-
-  const setinputdetail = (event) => {
-    console.log("setinputdetail", event.target.value);
-    setinputdetailmbl(event.target.value);
-  };
-
-  const setinputtahun = (event) => {
-    console.log("setinputtahun", event.target.value);
-    setinputtahunmbl(event.target.value);
-  };
-
-  const setinputtipe = (event) => {
-    console.log("setinputtipe", event.target.value);
-    setinputtipembl(event.target.value);
-  };
-
-  const setinputmesin = (event) => {
-    console.log("setinputmesin", event.target.value);
-    setinputmesinmbl(event.target.value);
-  };
-
-  const setinputtransmisi = (event) => {
-    console.log("setinputtransmisi", event.target.value);
-    setinputtransmisimbl(event.target.value);
-  };
-
-  // React.useEffect(() => {
-  //   fetchlist();
-  // }, []);
+  React.useEffect(() => {
+    fetchlist();
+  }, []);
 
   const fetchlist = async () => {
     setLoading(true);
     const respdatas = await CarListReady();
-    console.log(respdatas);
+    // console.log(respdatas);
     setlistdatatosell(respdatas);
     setLoading(false);
   };
 
   const setfilteredyear = (data) => {
-    console.log("setfilteredyear", data.target.value);
+    // console.log("setfilteredyear", data.target.value);
     setfilteredyeardt(data.target.value);
   };
 
   const setmerekmobil = (data) => {
-    console.log("setmerekmobil", data.target.value);
+    // console.log("setmerekmobil", data.target.value);
     setmerekmobildt(data.target.value);
   };
 
   const sethargamobil = (data) => {
-    console.log("sethargamobil", data.target.value);
+    // console.log("sethargamobil", data.target.value);
     sethargamobildt(data.target.value);
   };
 
   const setwarnamobil = (data) => {
-    console.log("setwarnamobil", data.target.value);
+    // console.log("setwarnamobil", data.target.value);
     setwarnamobildt(data.target.value);
   };
 
@@ -117,18 +57,14 @@ const Index = () => {
       kendaraan_merk: merekmobildt === null ? "" : merekmobildt,
       kendaraan_warna: warnamobildt === null ? "" : warnamobildt,
     };
-    console.log("carimobil", reqbody);
+    // console.log("carimobil", reqbody);
     const x = await findmobildetail(reqbody);
     if (x.status === 200) {
       setlistdatatosell(x);
     } else {
       setlistdatatosell(null);
     }
-    console.log("carimobil", x);
-  };
-
-  const cekharga = async () => {
-    console.log("Tampilkan Harga");
+    // console.log("carimobil", x);
   };
 
   if (loading) return <Oval height={80} width={80} color="#DB3E53" wrapperStyle={styling.loading} wrapperClass="" visible={true} ariaLabel="oval-loading" secondaryColor="white" strokeWidth={2} strokeWidthSecondary={2} />;
@@ -210,123 +146,8 @@ const Index = () => {
           ))}
       </div>
       <div style={{ padding: 15 }}></div>
-      <div
-        style={{
-          border: "1px solid black",
-          // backgroundColor: "#DB3E53",
-          borderRadius: 8,
-          textAlign: "center",
-          fontSize: 25,
-          marginTop: "2rem",
-        }}
-      >
-        INGIN JUAL MOBILMU? CEK HARGA DISINI GRATIS!!
-        <div
-          style={{
-            marginTop: "1rem",
-            fontWeight: "lighter",
-          }}
-        >
-          Harap Masukkan Data Mobilmu Dibawah Sini
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            padding: "4rem",
-            justifyContent: "center",
-            backgroundColor: "white",
-          }}
-        >
-          <FormControl variant="standard" sx={{ minWidth: 194 }}>
-            <InputLabel>Merek Mobil</InputLabel>
-            <Select value={inputmerekmbl} onChange={(value) => setinputmerek(value)} label="platMobil">
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              {sampledata.map((i, x) => (
-                <MenuItem key={x} value={i}>
-                  {i.merek}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl variant="standard" sx={{ minWidth: 194 }}>
-            <InputLabel>Detail Mobil</InputLabel>
-            <Select value={inputdetailmbl} onChange={(value) => setinputdetail(value)} label="platMobil">
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              {detail_merek.map((i, x) => (
-                <MenuItem key={x} value={i}>
-                  {i.detail_merek}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl variant="standard" sx={{ minWidth: 194 }}>
-            <InputLabel>Tahun Mobil</InputLabel>
-            <Select value={inputtahunmbl} onChange={(value) => setinputtahun(value)} label="platMobil">
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              {detail_spek.map((i, x) => (
-                <MenuItem key={x} value={i}>
-                  {i.tahun}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl variant="standard" sx={{ minWidth: 194 }}>
-            <InputLabel>Tipe Mobil</InputLabel>
-            <Select value={inputtipembl} onChange={(value) => setinputtipe(value)} label="platMobil">
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              {detail_spek.map((i, x) => (
-                <MenuItem key={x} value={i}>
-                  {i.tipe}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl variant="standard" sx={{ minWidth: 194 }}>
-            <InputLabel>Jenis Mesin</InputLabel>
-            <Select value={inputmesinmbl} onChange={(value) => setinputmesin(value)} label="platMobil">
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              {detail_spek.map((i, x) => (
-                <MenuItem key={x} value={i}>
-                  {i.mesin}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl variant="standard" sx={{ minWidth: 194 }}>
-            <InputLabel>Transmisi</InputLabel>
-            <Select value={inputtransmisimbl} onChange={(value) => setinputtransmisi(value)} label="platMobil">
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              {detail_spek.map((i, x) => (
-                <MenuItem key={x} value={i}>
-                  {i.transmisi}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <div
-            style={{
-              marginTop: "3rem",
-            }}
-          >
-            <Button type={"button"} onClick={() => cekharga()} variant="contained" size="large">
-              Cek Harga
-            </Button>
-          </div>
-        </div>
-      </div>
+
+      <SellCar />
     </Bodywrapper>
   );
 };
